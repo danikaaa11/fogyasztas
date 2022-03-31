7061c04be048d9864c7e97647e5fc18240615226
danikaaa11 <93983036+danikaaa11@users.noreply.github.com>
2022-03-31 14:24:37 +0200

    update 2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function szamol () {
 let F =document.getElementById('fogyasztas');
 let V =document.getElementById('tank');
 let Ut =document.getElementById('ut');
+
+let megteheto = V * 100/ F ;
+
+if (megteheto<Ut){
+    document.getElementById('megoldas').innerHTML = "az út során tankolni kell";
+
+    document.getElementById('megoldás').innerHTML = "Az út megtehető tankolás nélkül"
+}
+}
