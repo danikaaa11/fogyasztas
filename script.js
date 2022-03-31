e1271e17e694101d4b5ccc22a049f4639807288c
danikaaa11 <93983036+danikaaa11@users.noreply.github.com>
2022-03-31 13:25:05 +0200

    repair

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let F =document.getElementById˙('fogyasztas');
-let V =document.getElementById˙('tank');
-let Út =document.getElementById˙('út');
+let F =document.getElementById('fogyasztas');
+let V =document.getElementById('tank');
+let Ut =document.getElementById('ut');
