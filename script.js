0646bcbdf7af5f576d8117ebd96d7c7999636fd0
danikaaa11 <93983036+danikaaa11@users.noreply.github.com>
2022-04-07 12:34:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let megteheto = V * 100/ F ;
 
 if (megteheto<Ut){
     document.getElementById('megoldas').innerHTML = "az út során tankolni kell";
-
+ }
+ {
     document.getElementById('megoldás').innerHTML = "Az út megtehető tankolás nélkül"
-}
+ }
 }
